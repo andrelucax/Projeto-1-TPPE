d1bbf04c5bf53b233f2d35721668e0b349013542
Andre Lucas de Sousa Pinto <andrelucax@gmail.com>
2020-11-19 19:46:16 -0300

    Criacao da excecao ArquivoNaoEncontradoException da leitura do arquivo
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

---
+++ src/expts/ArquivoNaoEncontradoException.java
@@ -0,0 +1,14 @@
+package expts;
+
+public class ArquivoNaoEncontradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ArquivoNaoEncontradoException(String msg){
+        super("Nao foi possivel abrir o arquivo: " + msg);
+	}
+
+}

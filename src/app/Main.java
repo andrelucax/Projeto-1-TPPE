201f5828dc82e937beeaf52ff88c3112645ab038
Welison Almeida <welison.almeida.923@gmail.com>
2020-11-21 20:29:06 -0300

    Definiçao da funçao principal da aplicaçao (Main).
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>

---
+++ src/app/Main.java
@@ -0,0 +1,69 @@
+package app;
+
+import java.util.Scanner;
+
+import expts.ArquivoNaoEncontradoException;
+import expts.DelimitadorInvalidoException;
+import expts.EscritaNaoPermitidaException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Parser parser = new Parser();
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite a localização do arquivo de entrada: ");
+		String caminhoArquivoEntrada = scanner.nextLine();
+
+		try {
+			parser.lerArquivo(caminhoArquivoEntrada);
+		} catch (ArquivoNaoEncontradoException e) {
+			System.out.println(e);
+			System.exit(-1);
+		}
+		
+		System.out.print("Digite o delimitador do arquivo de saída: ");
+		String delimitadorArquivoSaida = scanner.nextLine();
+		
+		try {
+			parser.setDelimitador(delimitadorArquivoSaida);
+		} catch (DelimitadorInvalidoException e) {
+			System.out.println(e);
+			System.exit(-1);
+		}
+		
+		System.out.print("Digite o formato do arquivo de saída (0 - Linha, 1 - Coluna): ");
+		int formatoArquivoSaida = scanner.nextInt();
+		scanner.nextLine();
+
+		if (formatoArquivoSaida == 0 || formatoArquivoSaida == 1) {
+			parser.setFormatoSaida(formatoArquivoSaida);	
+		} else {
+			System.out.println("Formato de saída inválido.");
+			System.exit(-1);
+		}
+		
+		System.out.print("Digite o caminho da pasta para salvar o arquivo de saída: ");
+		String caminhoArquivoSaida = scanner.nextLine();
+
+		try {
+			parser.setCaminhoArquivoSaida(caminhoArquivoSaida);
+		} catch (EscritaNaoPermitidaException e) {
+			System.out.println(e);
+			System.exit(-1);
+		}
+
+		try {
+			parser.escreverArquivo();
+		} catch (EscritaNaoPermitidaException e) {
+			System.out.println(e);
+			System.exit(-1);
+		}
+		
+		System.out.println("\n>> Resultado final salvo na pasta informada.");
+		System.out.println(">> Programa encerrado!");
+
+		scanner.close();
+	}
+
+}

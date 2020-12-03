610160ae5ce42ef7fbb7d9d293bd4e64832657cb
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2020-12-03 19:54:07 -0300

    [Refact] Substituir Método por Método-objeto Origem: Parser.java Impactos: Parser.java, EscreverArquivo.java Explicação: a refatoração baseou-se na substituição do trecho de código que escreve no arquivo de saída de forma a transformá-lo em um Método-objeto.
    Co-authored-by: Andre Lucas <andrelucax@gmail.com>
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>

--- src/app/Parser.java
+++ src/app/Parser.java
@@ -1,10 +1,8 @@
 package app;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -55,9 +53,9 @@ public class Parser {
 		input.close();
 		int lastIndex = path.lastIndexOf('/');
 		if(lastIndex != -1) {
-			this.nomeArquivoEntrada = path.substring(lastIndex+1);
+			this.setNomeArquivoEntrada(path.substring(lastIndex+1));
 		} else {
-			this.nomeArquivoEntrada = path;
+			this.setNomeArquivoEntrada(path);
 		}
 	}
 
@@ -127,7 +125,7 @@ public class Parser {
 	}
 
 	public void escreverArquivo() throws EscritaNaoPermitidaException {
-		String caminhoCompletoSaida = this.nomeArquivoEntrada;
+		String caminhoCompletoSaida = getNomeArquivoEntrada();
 
 	    int indexPonto = -1;
 	    for(int i = caminhoCompletoSaida.length() - 1; i >= 0; i--) {
@@ -145,53 +143,14 @@ public class Parser {
 	        caminhoCompletoSaida += "Tab";
 	    }
 
-	    caminhoCompletoSaida = caminhoArquivoSaida + caminhoCompletoSaida;
-	    this.arquivoSaida = caminhoCompletoSaida;
+	    caminhoCompletoSaida = getCaminhoArquivoSaida() + caminhoCompletoSaida;
+	    setArquivoSaida(caminhoCompletoSaida);
 	    
-	    try {
-	    	File file = abrirArquivoSaida(caminhoCompletoSaida);
-		    
-	    	FileWriter filew = new FileWriter(file);
-	    	BufferedWriter bufferw = new BufferedWriter(filew);
-		    
-		    if(this.formato == LINHA) {
-		    	for(int index = 0; index < this.buffer.size(); index++) {
-		    		if(index != 0) bufferw.newLine();
-		    		bufferw.write(Integer.toString(index+1));
-		    		for (int i=0; i < this.buffer.elementAt(index).size(); i++) {
-		    			bufferw.write(this.delimitador);
-		    			bufferw.write(Integer.toString(buffer.elementAt(index).elementAt(i)));
-		    		}
-		    	}
-		    } else if(this.formato == COLUNA) {
-		    	int max_size=0;
-		    	for(int i=0; i<buffer.size(); i++) {
-		    		if(i!=0) bufferw.write(this.delimitador);
-		    		bufferw.write(Integer.toString(i+1));
-		    		if(buffer.elementAt(i).size() > max_size) max_size = buffer.elementAt(i).size();
-		    	}
-		    	bufferw.newLine();
-		    	for(int j=0; j<max_size; j++) { // linha
-		    		if(j!=0) bufferw.newLine();
-		    		for(int i=0; i<buffer.size(); i++) {
-		    			if(i!=0) bufferw.write(this.delimitador);
-		    			if(buffer.elementAt(i).size() > j) {
-		    				bufferw.write(Integer.toString(buffer.elementAt(i).elementAt(j)));
-		    			}
-		    		}
-		    	}
-		    	
-		    }
-		    
-		    bufferw.close();
-		    filew.close();
-	    } catch (Exception e) {
-	    	System.out.println(e);
-	    	throw new EscritaNaoPermitidaException(caminhoCompletoSaida);
-	    }
+		new EscreverArquivo(this).escreve();
+		return;
 	}
 
-	private File abrirArquivoSaida(String caminhoCompletoSaida) throws IOException {
+	public File abrirArquivoSaida(String caminhoCompletoSaida) throws IOException {
 		File file = new File(caminhoCompletoSaida);
 		if(!file.exists()) {
 			file.createNewFile();
@@ -201,6 +160,18 @@ public class Parser {
 
 	public String getArquivoSaida() {
 		return arquivoSaida;
+	}
+
+	public String getNomeArquivoEntrada() {
+		return nomeArquivoEntrada;
+	}
+
+	public void setNomeArquivoEntrada(String nomeArquivoEntrada) {
+		this.nomeArquivoEntrada = nomeArquivoEntrada;
+	}
+
+	public void setArquivoSaida(String caminhoCompletoSaida) {
+		this.arquivoSaida = caminhoCompletoSaida;
 	}
 	
 }
